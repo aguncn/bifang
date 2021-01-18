f9b6790750004b7cdd012195c56a2270bf112492
pkev1n <pkev1n@pkev1ndeMacBook-Pro.local>
2021-01-18 23:32:21 +0800

    增加新页面

---
+++ bifangfront/src/views/deployment/startup.vue
@@ -0,0 +1,189 @@
+
+<template>
+  <a-card>
+    <div class="search">
+      <a-form layout="horizontal">
+        <div class="fold">
+          <a-row >
+          <a-col :md="8" :sm="24" >
+            <a-form-item
+              label="项目"
+              :labelCol="{span: 5}"
+              :wrapperCol="{span: 18, offset: 1}"
+            >
+              <a-input placeholder="请输入" />
+            </a-form-item>
+          </a-col>
+          <a-col :md="8" :sm="24" >
+            <a-form-item
+              label="组件"
+              :labelCol="{span: 5}"
+              :wrapperCol="{span: 18, offset: 1}"
+            >
+              <a-select placeholder="请选择">
+                <a-select-option value="1">关闭</a-select-option>
+                <a-select-option value="2">运行中</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :md="8" :sm="24" >
+            <a-form-item
+              label="组件状态"
+              :labelCol="{span: 7}"
+              :wrapperCol="{span: 16,offset: 1}"
+            >
+              <a-input placeholder="请输入" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        </div>
+        <span style="float: right; margin-top: 3px;">
+          <a-button type="primary">查询</a-button>
+          <a-button style="margin-left: 8px">重置</a-button>
+        </span>
+      </a-form>
+    </div>
+    <div>
+      <bf-table
+        :columns="columns"
+        :dataSource="dataSource"
+        :selectedRows.sync="selectedRows"
+        @clear="onClear"
+        @change="onChange"
+        @selectedRowChange="onSelectChange"
+      >
+        <div slot="description" slot-scope="{text}">
+          {{text}}
+        </div>
+        <div slot="action" slot-scope="{text, record}">
+          <a style="margin-right: 8px">
+            <a-icon type="plus"/>FAT
+          </a>
+          <a style="margin-right: 8px">
+            <a-icon type="edit"/>UAT
+          </a>
+          <a @click="deleteRecord(record.key)">
+            <a-icon type="delete" />PRD
+          </a>
+          <a @click="deleteRecord(record.key)" v-auth="`delete`">
+            <a-icon type="delete" />DRP
+          </a>
+        </div>
+        <template slot="statusTitle">
+          <a-icon @click.native="onStatusTitleClick" type="info-circle" />
+        </template>
+      </bf-table>
+    </div>
+  </a-card>
+</template>
+
+<script>
+import BfTable from '@/components/table/table'
+const columns = [
+  {
+    title: '发布单编号',
+    dataIndex: 'no'
+  },
+  {
+    title: '组件',
+    dataIndex: 'description',
+    scopedSlots: { customRender: 'description' }
+  },
+  {
+    title: '容器模板',
+    dataIndex: 'callNo'
+  },
+  {
+    title: '启停操作',
+    scopedSlots: { customRender: 'action' }
+  }
+]
+
+const dataSource = []
+
+for (let i = 0; i < 100; i++) {
+  dataSource.push({
+    key: i,
+    no: 'NO ' + i,
+    description: '组件'+i,
+    callNo: 'docker'+Math.floor(Math.random() * 1000),
+    status: Math.ceil(Math.random()*2),
+    updatedAt: '2020-07-26'
+  })
+}
+
+export default {
+  name: 'serviceStartup',
+  components: {BfTable},
+  data () {
+    return {
+      advanced: true,
+      columns: columns,
+      dataSource: dataSource,
+      selectedRows: []
+    }
+  },
+  authorize: {
+    deleteRecord: 'delete'
+  },
+  methods: {
+    deleteRecord(key) {
+      this.dataSource = this.dataSource.filter(item => item.key !== key)
+      this.selectedRows = this.selectedRows.filter(item => item.key !== key)
+    },
+    toggleAdvanced () {
+      this.advanced = !this.advanced
+    },
+    remove () {
+      this.dataSource = this.dataSource.filter(item => this.selectedRows.findIndex(row => row.key === item.key) === -1)
+      this.selectedRows = []
+    },
+    onClear() {
+      this.$message.info('您清空了勾选的所有行')
+    },
+    onStatusTitleClick() {
+      this.$message.info('你点击了状态栏表头')
+    },
+    onChange() {
+      this.$message.info('表格状态改变了')
+    },
+    onSelectChange() {
+      this.$message.info('选中行改变了')
+    },
+    addNew () {
+      this.dataSource.unshift({
+        key: this.dataSource.length,
+        no: 'NO ' + this.dataSource.length,
+        description: '这是一段描述',
+        callNo: Math.floor(Math.random() * 1000),
+        status: Math.floor(Math.random() * 10) % 4,
+        updatedAt: '2018-07-26'
+      })
+    },
+    handleMenuClick (e) {
+      if (e.key === 'delete') {
+        this.remove()
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .search{
+    margin-bottom: 54px;
+  }
+  .fold{
+    width: calc(100% - 216px);
+    display: inline-block
+  }
+  .operator{
+    margin-bottom: 18px;
+  }
+  @media screen and (max-width: 900px) {
+    .fold {
+      width: 100%;
+    }
+  }
+</style>
+
---
+++ bifangfront/src/views/server/add.vue
@@ -0,0 +1,95 @@
+<template>
+  <a-card :body-style="{padding: '24px 32px'}" :bordered="false">
+    <a-form>
+      <a-form-item
+        label="标题"
+        :labelCol="{span: 7}"
+        :wrapperCol="{span: 10}"
+      >
+        <a-input placeholder="给目标起个名字" />
+      </a-form-item>
+      <a-form-item
+        label="起止日期"
+        :labelCol="{span: 7}"
+        :wrapperCol="{span: 10}"
+      >
+        <a-range-picker style="width: 100%" />
+      </a-form-item>
+      <a-form-item
+        label="目标描述"
+        :labelCol="{span: 7}"
+        :wrapperCol="{span: 10}"
+      >
+        <a-textarea rows="4" placeholder="请输入你阶段性工作目标"/>
+      </a-form-item>
+      <a-form-item
+        label="衡量标准"
+        :labelCol="{span: 7}"
+        :wrapperCol="{span: 10}"
+      >
+        <a-textarea rows="4" placeholder="请输入衡量标准"/>
+      </a-form-item>
+      <a-form-item
+        label="客户"
+        :labelCol="{span: 7}"
+        :wrapperCol="{span: 10}"
+        :required="false"
+      >
+        <a-input placeholder="请描述你服务的客户，内部客户直接 @姓名／工号"/>
+      </a-form-item>
+      <a-form-item
+        label="邀评人"
+        :labelCol="{span: 7}"
+        :wrapperCol="{span: 10}"
+        :required="false"
+      >
+        <a-input placeholder="请直接 @姓名／工号，最多可邀请 5 人"/>
+      </a-form-item>
+      <a-form-item
+        label="权重"
+        :labelCol="{span: 7}"
+        :wrapperCol="{span: 10}"
+        :required="false"
+      >
+        <a-input-number :min="0" :max="100"/>
+        <span>%</span>
+      </a-form-item>
+      <a-form-item
+        label="目标公开"
+        :labelCol="{span: 7}"
+        :wrapperCol="{span: 10}"
+        :required="false"
+        help="客户、邀评人默认被分享"
+      >
+        <a-radio-group v-model="value">
+          <a-radio :value="1">公开</a-radio>
+          <a-radio :value="2">部分公开</a-radio>
+          <a-radio :value="3">不公开</a-radio>
+        </a-radio-group>
+      </a-form-item>
+      <a-form-item style="margin-top: 24px" :wrapperCol="{span: 10, offset: 7}">
+        <a-button type="primary">提交</a-button>
+        <a-button style="margin-left: 8px">保存</a-button>
+      </a-form-item>
+    </a-form>
+  </a-card>
+</template>
+
+<script>
+export default {
+  name: 'addServer',
+  data () {
+    return {
+      value: 1
+    }
+  },
+  computed: {
+    desc() {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
